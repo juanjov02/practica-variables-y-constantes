c0d567e05001e990a9f89d6bc4071bb35a6a22f8
juanjov02 <79020401+juanjov02@users.noreply.github.com>
2021-09-17 18:50:12 -0500

    Add files via upload

---
+++ js/ejercicio10.js
@@ -0,0 +1,3 @@
+let operando1 = 23;
+let operando1 = 33;
+//no es correcto porque al cambiar la asignacion no se ultiliza let
---
+++ js/ejercicio11.js
@@ -0,0 +1,3 @@
+let operando1 = 23;
+operando1 = 33;
+//si porque se le esta reasignando el valor
